9c5711c3bb5b37bb31d1c9d13407b85d81f3e0ea
alska95 <rhsalska55@naver.com>
2021-12-22 18:10:18 +0900

    webSocket config, controller 구현하기

---
+++ src/main/java/com/example/socket/config/WebSocketConfig.java
@@ -0,0 +1,26 @@
+package com.example.socket.config;
+
+import com.example.socket.handler.ChatHandler;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.socket.config.annotation.EnableWebSocket;
+import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
+import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
+
+
+/**
+ * EnableWebSocket으로 WebSocket활성화
+ * WebSocket에 접속하기 위한 endpoint는 /chat으로 설정한다.
+ * cross origin설정도 해준다.
+ * */
+@Configuration
+@RequiredArgsConstructor
+@EnableWebSocket
+public class WebSocketConfig implements WebSocketConfigurer {
+    private final ChatHandler chatHandler;
+
+    @Override
+    public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
+        registry.addHandler(chatHandler, "ws/chat").setAllowedOrigins("*");
+    }
+}
---
+++ src/main/java/com/example/socket/controller/ChatController.java
@@ -0,0 +1,16 @@
+package com.example.socket.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+@Slf4j
+public class ChatController {
+
+    @GetMapping("/chat")
+    public String receiveChat(){
+        log.info("ChatController message received");
+        return "chat";
+    }
+}
